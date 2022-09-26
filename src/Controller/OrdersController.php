8028a0de5adb7616996ee8dac5827b1cfb4a5e19
Marc-hdev <102926163+Mhenrydev@users.noreply.github.com>
2022-09-26 14:08:21 +0200

    toto

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Orders;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class OrdersController extends AbstractController
 {
-    #[Route('/orders', name: 'app_orders')]
-    public function index(): Response
+    #[Route('/order', name: 'app_orders')]
+    public function index(Request $request): Response
     {
+        $orderLines = [];
+
+        $order = new Orders();
+        $order->setAmount($request->request->get('amount'));
+        $order->setUserId($request->request->get('user_id'));
+        $order->setCreatedAt(null);
+        $order->setStatus('panier');
+
+        var_dump($order);
         return $this->render('orders/index.html.twig', [
             'email' => 'test@test.fr',
             'idOrder' => 1,
