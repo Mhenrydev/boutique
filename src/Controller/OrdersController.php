41f8afab60389446552009db90e22c6199199675
Morad Zahid <morad.zahid@laposte.net>
2022-09-26 11:58:31 +0200

    create payment area

---
+++ src/Controller/OrdersController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrdersController extends AbstractController
+{
+    #[Route('/orders', name: 'app_orders')]
+    public function index(): Response
+    {
+        return $this->render('orders/index.html.twig', [
+            'email' => 'test@test.fr',
+            'idOrder' => 1,
+            'items' => 3,
+            'amount' => 100
+        ]);
+    }
+}
